1472b230fcb9a34ff9854a4c748b4a81b2c26ffb
Hussein Kadweka <developersjuniors@gmail.com>
2019-10-17 02:06:38 +0300

    imported both our forms and services to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import{ ServiceRequestService } from './service-http/service-request.service'
+import { RouterModule,Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DateCountPipe } from './date-count.pipe';
 import { GithubDetailsComponent } from './github-details/github-details.component';
+
+const routes:Routes =[
+  {path:'',redirectTo:'/search-details',pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    DateCountPipe,
     GithubDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ServiceRequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
